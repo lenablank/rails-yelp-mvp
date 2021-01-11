0a6bd20b24785c9aa6b78073fa61ae0fdbb17b1f
Lena Blank <mine.blank@gmail.com>
2021-01-11 17:17:45 +0100

    seeded 12 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants'
+12.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  puts "restaurant #{restaurant.id} is created"
+end
+
+puts 'done'
